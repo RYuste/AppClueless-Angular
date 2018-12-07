257b009c4483cb828959ac6586841ce9dc9fe068
Rafael Yuste <teka_ryj@hotmail.com>
2018-12-07 15:16:58 +0100

    Corrección de errores

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAAcy4f8LaAzr8YgH5rboWshf0DXvdK-DM",
+    authDomain: "appclueless.firebaseapp.com",
+    databaseURL: "https://appclueless.firebaseio.com",
+    projectId: "appclueless",
+    storageBucket: "appclueless.appspot.com",
+    messagingSenderId: "977919325487"
+  }
 };
